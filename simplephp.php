0f95a0dc61078c7fd077dad646e630a078e84847
fajar757 <cyberrose9@gmail.com>
2017-03-21 15:31:53 +0700

    Delete simplephp.php

--- simplephp.php
+++
@@ -1,7 +0,0 @@
-<?php
-$xml=simplexml_load_file("mahasiswa.xml") or die("Error: Cannot create object");
-echo $xml->mahasiswa[0]->nim . "<br>";
-echo $xml->mahasiswa[0]->nama . "<br>";
-echo $xml->mahasiswa[0]->alamat . "<br>";
-echo $xml->mahasiswa[0]->progdi . "<br>";
-?> 
